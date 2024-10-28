9c0796112e2e7bba64178181022e49a2381fc18f
Jafet Isidoro <jafetcodes@gmail.com>
2024-10-28 15:50:50 -0500

    MONDAY

--- week6/hw_hangman/controllers/game.js
+++ week6/hw_hangman/controllers/game.js
@@ -1,4 +1,4 @@
-const Words = require("../models/Words"); // Adjusted path
+const Words = require("../models/Words");
 const game = {};
 const MAX_GUESS = 7;
 let numWrong = 0;
@@ -31,8 +31,6 @@ exports.newGame = (req, res) => {
     let startWord = word.getRandomWord();
     let hiddenWord = "";
     game.startWord = startWord;
-    // game.numRight = 0;
-    // game.numWrong = 0;
     console.log(startWord);
 
     for (let i = 0; i < startWord.word.length; i++) {
@@ -54,27 +52,23 @@ exports.newGame = (req, res) => {
 exports.getLetter = (req, res) => {
     let chosenChar = req.body.character;
     let letterFound = false;
+    let ctr = 0;
     let remainingGuess = 7;
     let numWrong = 0;
     console.log(chosenChar);
     if (!game.startWord || !game.startWord.word) {
         return res.redirect('/');
     }
-    for (let i = 0; i < game.startWord.word.length; i++) {
-        if (game.startWord.word[i] === chosenChar){
-            let letterFound = true;
+    while (ctr < game.startWord.word.length && !letterFound) {
+        if (game.startWord.word[ctr] === chosenChar) {
+            letterFound = true;
         }
+        ctr++;
     }
-    if (!letterFound) {
-        remainingGuess -= 1;
-        res.render('gamePlay', {
-            wrongCount: numWrong += 1,
-            // guesses: remainingGuess,
-            guesses: remainingGuess-=1,
-            word: chosenChar,
-        })
-    }
-    else{
+
+    // for (let i = 0; i < game.startWord.word.length; i++) {
+
+    if (letterFound) {
         console.log(game.startWord);
         remainingGuess -= 1;
         res.render('gamePlay', {
@@ -82,9 +76,17 @@ exports.getLetter = (req, res) => {
             guesses: remainingGuess,
 
         })
+    } else {
+        numWrong += 1;
+        remainingGuess -= 1;
+        res.render('gamePlay', {
+            wrongCount: numWrong,
+            // guesses: remainingGuess,
+            guesses: remainingGuess,
+            word: chosenChar,
+        })
     }
 }
-
 // exports.getLetter = (req, res) => {
 //     // Check if game is initialized
 //     if (!game.startWord || !game.startWord.word) {
